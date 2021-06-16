605133bd6218aa7eb069c3fcae23d3a6ec52288c
Gerson Junior <gerson.junior@gmz.solutions>
2021-06-15 21:49:18 -0300

    feat: creating save on controller

--- src/main/java/com/apirest/octoevents/controller/IssueController.java
+++ src/main/java/com/apirest/octoevents/controller/IssueController.java
@@ -1,5 +1,23 @@
 package com.apirest.octoevents.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apirest.octoevents.entity.Issue;
+import com.apirest.octoevents.services.IssueService;
+
+import reactor.core.publisher.Mono;
+
+@RestController
 public class IssueController {
 
+	@Autowired
+	IssueService issueService;
+	
+	@PostMapping(value="/issues")
+	public Mono<Issue> save(@RequestBody Issue issue){
+		return issueService.save(issue);
+	}
 }
