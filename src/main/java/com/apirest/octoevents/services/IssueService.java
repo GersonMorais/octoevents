b6d188f69c569187e6e69e9dff55631d39aecc98
Gerson Junior <gerson.junior@gmz.solutions>
2021-06-15 21:35:29 -0300

    feat: creating service structure to save issue

--- src/main/java/com/apirest/octoevents/services/IssueService.java
+++ src/main/java/com/apirest/octoevents/services/IssueService.java
@@ -1,5 +1,11 @@
 package com.apirest.octoevents.services;
 
+import com.apirest.octoevents.entity.Issue;
+
+import reactor.core.publisher.Mono;
+
 public interface IssueService {
 
+	Mono<Issue> save(Issue issue);
+	
 }
--- src/main/java/com/apirest/octoevents/services/IssueServiceImpl.java
+++ src/main/java/com/apirest/octoevents/services/IssueServiceImpl.java
@@ -1,5 +1,22 @@
 package com.apirest.octoevents.services;
 
-public class IssueServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.apirest.octoevents.entity.Issue;
+import com.apirest.octoevents.repository.IssueRepository;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class IssueServiceImpl implements IssueService {
+
+	@Autowired
+	IssueRepository issueRepository;
+	
+	@Override
+	public Mono<Issue> save(Issue issue) {
+		return issueRepository.save(issue);
+	}
 
 }
