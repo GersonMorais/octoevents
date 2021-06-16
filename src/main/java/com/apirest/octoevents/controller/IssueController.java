76fe0f16ac24d9793b37e39ec12326f5408e693e
Gerson Junior <gerson.junior@gmz.solutions>
2021-06-16 01:18:53 -0300

    docs: created the application swagger

--- src/main/java/com/apirest/octoevents/controller/IssueController.java
+++ src/main/java/com/apirest/octoevents/controller/IssueController.java
@@ -2,6 +2,7 @@ package com.apirest.octoevents.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.apirest.octoevents.entity.Issue;
 import com.apirest.octoevents.services.IssueService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import reactor.core.publisher.Mono;
 
 @RestController
+@Api(value="API REST OctoEvents")
+@CrossOrigin(origins = "*")
 public class IssueController {
 
 	@Autowired
@@ -20,6 +25,7 @@ public class IssueController {
 	
 	@PostMapping(value="/issues")
 	@ResponseStatus(HttpStatus.CREATED)
+	@ApiOperation(value="Save the issues from the repository")
 	public Mono<Issue> save(@RequestBody Issue issue){
 		return issueService.save(issue);
 	}
